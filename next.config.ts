398ab1c0d515dddc8e3f31c7fe6259f344483a4e
Dev <pattnaikd833@gmail.com>
2025-12-01 12:38:08 +0530

    feat: Add Next.js `basePath` configuration.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/Anubhuti-renewable-energy-private-limited',
   images: {
     unoptimized: true,
   },
